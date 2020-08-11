af774478b42cb97e13667847683620971aca374f
VENOM-InstantDeath <mohathepro111@gmail.com>
2020-08-11 19:42:32 -0300

    added hcut package manager

---
+++ hcut.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+#hcut (o hkpac) - Gestor de paquetes para el repositorio Hacking-Utils.
+#Escrito por Darth Venom y Tobi Emotic.
+#< = >
+
+
+# Definición de la función main(). En Python esta función no es necesaria.
+
+def main():
+    pass;
+
+
+# if que comprueba si el ejecutor del programa es el programa en sí o si está siendo ejecutado como módulo o desde otro programa. Si el código está siendo ejecutado por el propio programa, entonces se ejecuta el interior del if (ingresando a la función main()).
+
+if __name__ == '__main__':
+
+    #llamada a la función main()
+    main();
